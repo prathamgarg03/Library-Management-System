971e67326902cfe9bd9e6718cbe99eb3149b55da
Gourav-Sharma243 <147566196+Gourav-Sharma243@users.noreply.github.com>
2023-10-10 17:22:53 -0700

    Delete studentHome directory

--- studentHome/studentHome.js
+++
@@ -1,7 +0,0 @@
-const button1 = document.querySelector("#searchButton");
-button1.onclick = goToSearchPage;
-
-function goToSearchPage(){
-    const path = "../student_search/student_Search.html";
-    window.open(path, "_blank");
- }
