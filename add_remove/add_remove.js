934c31b7b157d53d8771dfe2aed6af31c56a1cff
prathamgarg03 <89456233+prathamgarg03@users.noreply.github.com>
2023-10-10 19:44:06 -0700

    Update add_remove.js

--- add_remove/add_remove.js
+++ add_remove/add_remove.js
@@ -1,52 +1,10 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyByusMHtNMMtKsHcbsatmwgymD9nlwuIu0",
-    authDomain: "library-management-syste-9b904.firebaseapp.com",
-    databaseURL: "https://library-management-syste-9b904-default-rtdb.firebaseio.com",
-    projectId: "library-management-syste-9b904",
-    storageBucket: "library-management-syste-9b904.appspot.com",
-    messagingSenderId: "579021273060",
-    appId: "1:579021273060:web:65b743966ea659026eb9f1",
-    measurementId: "G-X410SE23SB"
-  };
+var arrayOfBooks = [];
 
-
-firebase.initializeApp(firebaseConfig);
-
-var lms = firebase.database().ref("library-management-syste");
-
-document.getElementById('add_form').onclick(submitAddForm);
-
-function submitAddForm() {
-    var name = getValue('name');
-    var author = getValue('author');
-    var publisher = getValue('publisher');
-    var quantity = getValue('quantity');
-
-    // console.log(name, author, publisher, quantity);
-    saveElm(name, author, publisher,  quantity); 
-}
-
-const saveElm = (name, author, publisher, quantity) => {
-    var newForm = lms.push();
-    
-    newForm.set({
-        name : name,
-        author : author,
-        publisher : publisher,
-        quantity : quantity,
-    });
-
-}
-const getValue = (id) => {
-    return document.getElementById(id).value;
-}
-var arrayOfBooks =[];
-
-function DisplayTable(){
+function DisplayTable() {
 
 }
 
-DisplayTable.prototype.add = function(tempBook) {
+DisplayTable.prototype.add = function (tempBook) {
     tableB = document.getElementById('tableB');
     let tempStr = ` <tr>
                         <td>${tempBook.name}</td>
@@ -57,14 +15,14 @@ DisplayTable.prototype.add = function(tempBook) {
     tableB.innerHTML += tempStr;
 }
 
-DisplayTable.prototype.clear = function(){
+DisplayTable.prototype.clear = function () {
     add_form = document.getElementById('add_form');
     add_form.reset();
 }
 
 
 class Book {
-    constructor(name,author,publisher,quantity) {
+    constructor(name, author, publisher, quantity) {
         this.name = name;
         this.author = author;
         this.publisher = publisher;
@@ -72,9 +30,9 @@ class Book {
     }
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const add_button = document.querySelector("#add_button");
-    add_button.addEventListener('click', function(evt) {
+    add_button.addEventListener('click', function (evt) {
         evt.preventDefault();
         addBook();
     });
