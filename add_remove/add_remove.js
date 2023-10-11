8e7e183c4b0182741c6c88ccbd0288649cd72dce
DuaRuben <147565092+DuaRuben@users.noreply.github.com>
2023-10-10 18:53:30 -0700

    Update add_remove.js

--- add_remove/add_remove.js
+++ add_remove/add_remove.js
@@ -57,12 +57,7 @@ function addBook() {
     //tempDisplay.clear();
 }
 
-const scrollLink = document.getElementById("#scrolldown");
 
-window.addEventListener("scroll", function() {
-    const scrollPosition = window.scrollY;
-    scrollLink.style.bottom = (scrollPosition >= 100) ? "20px" : "-50px";
-});
 
 
 
