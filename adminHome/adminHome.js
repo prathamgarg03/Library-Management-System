b028fc1b560bb888cdce7f597dc8514799b526d7
DuaRuben <147565092+DuaRuben@users.noreply.github.com>
2023-10-10 17:31:22 -0700

    Update adminHome.js

--- adminHome/adminHome.js
+++ adminHome/adminHome.js
@@ -3,5 +3,5 @@ button1.onclick = goToAddRemovePage;
 
 function goToAddRemovePage(){
    const path = "../add_remove/add_remove.html";
-   window.open(path, "_blank");
+   window.open(path, "_self");
 }
