8c2c995218497f1809509e8956f61c5bb6a76bc1
DuaRuben <147565092+DuaRuben@users.noreply.github.com>
2023-10-10 17:31:57 -0700

    Update studentHome.js

--- studentHome/studentHome.js
+++ studentHome/studentHome.js
@@ -3,5 +3,5 @@ button1.onclick = goToSearchPage;
 
 function goToSearchPage(){
     const path = "../student_search/student_Search.html";
-    window.open(path, "_blank");
+    window.open(path, "_self");
  }
