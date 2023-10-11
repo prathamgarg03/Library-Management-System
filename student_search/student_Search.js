1bc9ffd7480fc17d0c5bdd6ac06d789406e07423
DuaRuben <147565092+DuaRuben@users.noreply.github.com>
2023-10-10 18:52:47 -0700

    Update student_Search.js

--- student_search/student_Search.js
+++ student_search/student_Search.js
@@ -1,6 +1,1 @@
-const scrollLink = document.getElementById("#scrolldown");
 
-window.addEventListener("scroll", function() {
-    const scrollPosition = window.scrollY;
-    scrollLink.style.bottom = (scrollPosition >= 100) ? "20px" : "-50px";
-});
