8fadf3c785e8d56fcb8204a1253d29f53e45b31f
krishbedi17 <106929134+krishbedi17@users.noreply.github.com>
2023-10-11 00:59:57 -0700

    Update adminLogin.js

--- adminLogin/adminLogin.js
+++ adminLogin/adminLogin.js
@@ -5,38 +5,3 @@ function goToHomePage(){
    const path = "../adminHome/adminHome.html";
    window.open(path, "_self");
 }
-
-class Book{
-    constructor(name,author,publisher,quantity) {
-        this.name = name;
-        this.author = author;
-        this.publisher = publisher;
-        this.quantity = quantity;
-    }
-}
-
-const add_button = document.querySelector("#add_button");
-add_button.addEventListener('click', function(evt) {
-    evt.preventDefault();
-    addBook();
-});
-
-function addBook() {
-    let name = document.getElementById('name').value;
-    let author = document.getElementById('author').value;
-    let publisher = document.getElementById('publisher').value;
-    let quantity = document.getElementById('quantity').value;
-
-    let tempBook = new Book(name, author, publisher, quantity);
-    console.log(tempBook);
-
-    let tempDisplay = new displayTable();
-    tempDisplay.add(tempBook);
-    tempDisplay.clear();
-}
-    
-
-
-
-//add_button
-
