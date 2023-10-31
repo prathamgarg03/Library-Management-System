b037e1449fa4d134226a99e279474502c03e69b8
Gourav-Sharma243 <147566196+Gourav-Sharma243@users.noreply.github.com>
2023-10-31 12:55:06 -0700

    Update studentHome.js

--- studentHome/studentHome.js
+++ studentHome/studentHome.js
@@ -5,3 +5,11 @@ function goToSearchPage(){
     const path = "../student_search/student_Search.html";
     window.open(path, "_self");
  }
+
+ let loan=document.getElementById("loan_button");
+ function goToLoansPage(){
+     const path = "../Student_loan/Student_loan.html";
+     window.open(path, "_self");
+    }
+    
+    loan.onclick=goToLoansPage;
