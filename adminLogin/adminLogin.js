bd401d9bf9e5b6afb23a6c3fead9b652d6868a71
prathamgarg03 <gargpratham.312003@gmail.com>
2023-10-31 20:22:25 -0700

    student and admin signup and login

--- adminLogin/adminLogin.js
+++
@@ -1,7 +0,0 @@
-const button1 = document.querySelector("#LoginButton");
-button1.onclick = goToHomePage;
-
-function goToHomePage(){
-   const path = "../adminHome/adminHome.html";
-   window.open(path, "_self");
-}
