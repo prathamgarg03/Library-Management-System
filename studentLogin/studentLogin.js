6036ceb4845fa17d599552f469123c00dd167f79
prathamgarg03 <gargpratham.312003@gmail.com>
2023-10-31 20:10:23 -0700

    hvhj
    jhvjhv

--- studentLogin/studentLogin.js
+++
@@ -1,7 +0,0 @@
-const button1 = document.querySelector("#Studentloginbutton");
-button1.onclick = goToHomePage;
-
-function goToHomePage(){
-   const path = "../studentHome/studentHome.html";
-   window.open(path, "_self");
-}
